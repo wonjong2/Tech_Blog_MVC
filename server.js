7ad59ef2b89926764294c1e9ac6514f825d720ab
Wonjong Park <wonjong2@gmail.com>
2022-05-23 17:15:49 -0700

    Added a cookie into the session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 // Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -14,8 +15,17 @@ const PORT = process.env.PORT || 3001;
 // Set up sessions
 const sess = {
     secret: 'Super secret secret',
+    cookie: {
+        maxAge: 180,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },    
     resave: false,
     saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),    
 };
 
 app.use(session(sess));

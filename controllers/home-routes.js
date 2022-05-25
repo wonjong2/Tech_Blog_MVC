a18cc9881e5e9ce7a42b519e3371e4af218b93ff
Wonjong Park <wonjong2@gmail.com>
2022-05-25 13:01:57 -0700

    Changes for displaying page name underneath the navi menu

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
             }
         )
         const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('post', { posts, logged_in: req.session.logged_in });
+        res.render('post', { posts, logged_in: req.session.logged_in, page: 'The Tech Blog' });
     }
     catch (err) {
         console.log(err);
@@ -30,7 +30,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
             include: { model: Post, attributes: ['id', 'title', 'date'] }
         })
         const myPosts = myPostData.posts.map((post) => post.get({ plain: true }));
-        res.render('dashboard', { myPosts, logged_in: true })
+        res.render('dashboard', { myPosts, logged_in: true, page: 'Your Dashboard' });
     }
     catch (err) {
         res.status(500).json(err);
@@ -38,11 +38,11 @@ router.get('/dashboard', withAuth, async (req, res) => {
 });
 
 router.get('/login', async (req, res) => {
-    res.render('login');
+    res.render('login', { page: 'Your Dashboard' });
 });
 
 router.get('/signup', async (req, res) => {
-    res.render('signup');
+    res.render('signup', { page: 'The Tech Blog' });
 });
 
 module.exports = router;

73a23d0b65bc1ddeef8f3c4de82ddf277213b264
Wonjong Park <wonjong2@gmail.com>
2022-05-24 17:21:18 -0700

    Implements GET '/login' route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
@@ -13,13 +14,31 @@ router.get('/', async (req, res) => {
                 ]
             }
         )
-        const posts = postData.map((post) => post.get({plain: true}));
-        res.render('post',{posts, logged_in: req.session.loggedIn});
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('post', { posts, logged_in: req.session.loggedIn });
     }
     catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const myPostData = await Post.findAll({
+            where: { id: req.session.user }
+        })
+        const myPosts = myPostData.map((post) => post.get({ plain: true }));
+        res.render('dashboard', { myPosts, logged_in: true })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', async (req, res) => {
+    res.render('login');
 });
 
 module.exports = router;

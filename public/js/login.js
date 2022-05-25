1033706e54582c5400c080fd8a0a4ac8e2dad063
Wonjong Park <wonjong2@gmail.com>
2022-05-25 10:30:33 -0700

    Implements POST '/api/users/logout' route

--- public/js/login.js
+++ public/js/login.js
@@ -24,32 +24,6 @@ const loginFormHandler = async (event) => {
     }
 };
 
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
-
-//     const user_name = document.querySelector('#username-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
-
-//     console.log(user_name, password);
-//     if (user_name && password) {
-//         const response = await fetch('/api/users', {
-//             method: 'POST',
-//             body: JSON.stringify({ user_name, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-//         console.log(response);
-//         if (response.ok) {
-//             document.location.replace('/dashboard');
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-// };
-
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
-
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupFormHandler);
---
+++ public/js/logout.js
@@ -0,0 +1,19 @@
+const logoutHandler = async (event) => {
+    event.preventDefault();
+
+    console.log("logoutHandler");
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document
+    .querySelector('#logout')
+    .addEventListener('click', logoutHandler);

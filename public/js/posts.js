6084b2f7ceacde81162cdc4d620303b160e5a4f1
Wonjong Park <wonjong2@gmail.com>
2022-06-01 20:15:44 -0700

    Deletes console.log

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,9 +1,7 @@
 // Event handler for the clicking on the specific post in the home page
 const selectedPostHandler = async (event) => {
-    console.log(event);
     const id = event.target.dataset.postId ? event.target.dataset.postId : event.target.parentElement.dataset.postId;
 
-    console.log(id)
     if (id) {
         // Login state now, restart the timer to check auth.
         if (isLogin) {
